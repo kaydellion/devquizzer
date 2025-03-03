6ebf6cc7737f934461af5d42b5f1e853712bb0eb
kaydellion <kanyin500@gmail.com>
2025-03-03 03:03:39 +0100

    Remove session start and ID assignment from Google authentication flow

--- google-auth.php
+++ google-auth.php
@@ -50,8 +50,6 @@ if ($result->num_rows > 0) {
     $id = $con->insert_id;
     
     // Start session and set cookie
-    session_start();
-    $_SESSION['id'] = $id;
     setcookie("userID", $id, time() + (10 * 365 * 24 * 60 * 60));
 }
 
