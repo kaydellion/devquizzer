ebae3e8117b5b23580e6c142af0a0c03f45e5c6c
kaydellion <kanyin500@gmail.com>
2025-04-03 08:40:08 +0100

    footer2

--- footer.php
+++ footer.php
@@ -59,9 +59,6 @@
 
     <div class="container copyright text-center mt-4">
       <p>© <span>Copyright</span> <strong class="px-1 sitename">DevQuizzer</strong> <span>All Rights Reserved</span></p>
-      <div class="credits">
-        Designed by <a href="#" class="text-dark">Kayd</a>
-      </div>
     </div>
 
   </footer>
